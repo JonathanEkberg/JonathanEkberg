f49ccc8f5ad4f3311ed74786868290af53278abf
Jonathan Ekberg <jodrim@gmail.com>
2021-06-21 21:04:05 +0200

    Updated title and meta tags

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import { SunIcon, MoonIcon } from "@heroicons/react/outline"
-import { useEffect, useLayoutEffect, useState } from "react"
+import React, { useEffect, useLayoutEffect, useState } from "react"
 import Script from "next/script"
+import Head from "next/head"
 
 export default function Home() {
    const [darkmode, setDarkmode] = useState<boolean>(false)
@@ -34,33 +35,46 @@ export default function Home() {
    }
 
    return (
-      <main className="min-h-screen w-full grid place-items-center">
-         <button
-            onClick={toggleTheme}
-            className="fixed z-50 top-0 right-0 p-4 focus:outline-none"
-         >
-            {darkmode ? (
-               <SunIcon className="w-10 h-10 dark:text-gray-200" />
-            ) : (
-               <MoonIcon className="w-10 h-10 dark:text-gray-200" />
-            )}
-         </button>
-         <h1 id="name" className="font-black select-none">
-            Jonathan Ekberg
-            <style jsx>{`
-               #name {
-                  font-size: 10vw;
-                  background: -webkit-linear-gradient(
-                     ${darkmode
-                        ? "rgb(255, 255, 255), rgb(160, 160, 160)"
-                        : "rgb(96, 96, 96), rgb(0, 0, 0)"}
-                  );
-                  -webkit-background-clip: text;
-                  -webkit-text-fill-color: transparent;
-               }
-            `}</style>
-         </h1>
-      </main>
+      <>
+         <Head>
+            <title>Jonathan Ekberg</title>
+            <meta
+               name="description"
+               content="The official website of Jonathan Ekberg."
+            />
+            <meta
+               name="keywords"
+               content="jonathan ekberg, jonathan ekberg website, jonathan website, ekberg website, website"
+            />
+         </Head>
+         <main className="grid w-full min-h-screen place-items-center">
+            <button
+               onClick={toggleTheme}
+               className="fixed top-0 right-0 z-50 p-4 focus:outline-none"
+            >
+               {darkmode ? (
+                  <SunIcon className="w-10 h-10 dark:text-gray-200" />
+               ) : (
+                  <MoonIcon className="w-10 h-10 dark:text-gray-200" />
+               )}
+            </button>
+            <h1 id="name" className="font-black select-none">
+               Jonathan Ekberg
+               <style jsx>{`
+                  #name {
+                     font-size: 10vw;
+                     background: -webkit-linear-gradient(
+                        ${darkmode
+                           ? "rgb(255, 255, 255), rgb(160, 160, 160)"
+                           : "rgb(96, 96, 96), rgb(0, 0, 0)"}
+                     );
+                     -webkit-background-clip: text;
+                     -webkit-text-fill-color: transparent;
+                  }
+               `}</style>
+            </h1>
+         </main>
+      </>
    )
 }
 
